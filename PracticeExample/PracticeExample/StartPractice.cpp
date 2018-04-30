a36c0f52ba5c3b7b7f62ccc112260bc97c507ad5
grayleft <32466231+grayleft@users.noreply.github.com>
2018-05-01 00:55:00 +0800

    pratice02

--- PracticeExample/PracticeExample/StartPractice.cpp
+++ PracticeExample/PracticeExample/StartPractice.cpp
@@ -1,10 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "conditional.h"
+
+int max(int , int);
+
 int main()
 {
-	//printf("Hello World!");
-	hello();
-	getchar();
+	int GetNumber=0;
+	int i=0;
+	printf("Start!\n");
+	//hello();
+	//GetNumber=getchar();
+    scanf("%d",&GetNumber);
+	printf("GetNumber: %d\n",GetNumber);
 
+	(GetNumber%2==0)?printf("EvenNumber\n"):printf("OddNumber\n");
+    //JudgeOddOrEvenNumber£¨GetNumber£©;
+
+	for(i=0;i<10;i++)
+	{
+		printf("The i number : %d\n",i);
+	}
+
+	printf("max is %d\n",max(19,316));
+
+	system("pause");
 	return 0;
+}
+
+int max(int num1,int num2)
+{
+	int result=0;
+
+	if(num1<num2)
+		result=num2;
+	else
+		result=num1;
+
+	return result;
 }
--- PracticeExample/PracticeExample/conditional.h
+++ PracticeExample/PracticeExample/conditional.h
@@ -1,8 +1,6 @@
 #ifndef __conditional_h__
 #define __conditional_h__
 	
-	void hello(void)
-	{
-		printf("Hello World!\n");
-	}
+
+
 #endif
