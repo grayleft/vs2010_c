87f853c87b95bd7ba48e1dbb8cc158b6900904f9
grayleft <32466231+grayleft@users.noreply.github.com>
2018-05-02 00:14:55 +0800

    practice03

--- PracticeExample/PracticeExample/StartPractice.cpp
+++ PracticeExample/PracticeExample/StartPractice.cpp
@@ -3,29 +3,38 @@
 #include "conditional.h"
 
 int max(int , int);
+void PrintfArgument(char , char);
+void practice10(int,int);
 
-int main()
+int main(int argc,char *argv[])
 {
 	int GetNumber=0;
 	int i=0;
 	printf("Start!\n");
+	practice10(3,7);
 	//hello();
 	//GetNumber=getchar();
-    scanf("%d",&GetNumber);
-	printf("GetNumber: %d\n",GetNumber);
+    //scanf("%d",&GetNumber);
+	//printf("GetNumber: %d\n",GetNumber);
 
-	(GetNumber%2==0)?printf("EvenNumber\n"):printf("OddNumber\n");
+	//(GetNumber%2==0)?printf("EvenNumber\n"):printf("OddNumber\n");
     //JudgeOddOrEvenNumber£¨GetNumber£©;
 
-	for(i=0;i<10;i++)
-	{
-		printf("The i number : %d\n",i);
-	}
+	//for(i=0;i<10;i++)
+	//{
+	//	printf("The i number : %d\n",i);
+	//}
 
-	printf("max is %d\n",max(19,316));
+	//printf("max is %d\n",max(19,316));
 
 	system("pause");
 	return 0;
+}
+
+void PrintfArgument(char string1,char string2)
+{
+	printf("Program Name : %s\n",string1);
+	printf("Argument First is %s\n",string2);
 }
 
 int max(int num1,int num2)
@@ -38,4 +47,20 @@ int max(int num1,int num2)
 		result=num1;
 
 	return result;
+}
+
+void practice10(int i,int j)
+{	
+	int ascii1=1;
+	int itime,jtime;
+	printf("i: %d\t j: %d\n",i,j);
+	printf("%c\t%c\n",ascii1,ascii1);
+	for(itime=0;itime<i;itime++)
+	{
+		for(jtime=0;jtime<=itime;jtime++)
+		{
+			printf("1\t");
+		}
+		printf("\n");
+	}
 }
